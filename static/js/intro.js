28a20fb1797b228b7b0aeb5d63fc835c883742b3
Pablo Ricco <pricco@gmail.com>
2013-11-09 12:19:26 -0200

    Improve intro animation

--- static/js/intro.js
+++ static/js/intro.js
@@ -9,21 +9,29 @@
     debugMode: false,
     onready: function() {
       var t = (new Date()).getTime();
+      var backgroundImage = '/img/explosion.gif?t=' + t;
+      var logoImage = '/img/macgifer.png?t=' + t;
       soundManager.createSound({
         id: 1,
         url: '/sound/intro.mp3',
         autoLoad: true,
         onload: function(){
-          var explosion = new Image();
-          explosion.onload = function(){
+          var background = new Image();
+          background.onload = function(){
             var logo = new Image();
             logo.onload = function(){
+              //Chrome bug: background image doesn't load after refresh
+              var intro = document.createElement('div');
+              intro.className = 'intro';
               var background = document.createElement('div');
               background.className = 'background';
-              var div = document.createElement('div');
-              div.style.backgroundImage = 'url(/img/explosion.gif?t=' + t  + ')';
-              background.appendChild(div);
-              document.body.insertBefore(background, document.body.firstChild);
+              background.style.backgroundImage = 'url(' + backgroundImage  + ')';
+              intro.appendChild(background);
+              var logo = document.createElement('div');
+              logo.className = 'logo';
+              logo.style.backgroundImage = 'url(' + logoImage  + ')';
+              intro.appendChild(logo);
+              document.body.insertBefore(intro, document.body.firstChild);
               var volume = 100;
               soundManager.play(1, {volume: volume, from: 21000, to: 29000});
               setTimeout(function(){
@@ -38,9 +46,9 @@
                 }, 1);
               }, 5000);
             };
-            logo.src = '/img/macgifer.png?t=' + t;
+            logo.src = logoImage;
           };
-          explosion.src = '/img/explosion.gif?t=' + t;
+          background.src = backgroundImage;
         }
       });
     }
