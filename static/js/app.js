dd22ec9436bd13f6e9f4490fc9bf08ca20308976
Eduardo Veiga <ed.cinemascop@yahoo.es>
2013-11-08 23:40:27 -0200

    move app js

---
+++ static/js/app.js
@@ -0,0 +1,54 @@
+$(function(){
+  var frameRate = 10;
+  var width = 320;
+  var height = 240;
+
+  var video = $('video')[0];
+  video.width = width;
+  video.height = height;
+
+  var canvas = document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
+  var context = canvas.getContext('2d');
+  
+  var colorDiv = $('.color');
+  getUserMedia({video: true}, function(stream){
+    video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
+
+    renderTimer = setInterval(function() {
+      context.drawImage(video, 0, 0, video.width, video.height);
+      onFrame(canvas);
+    }, Math.round(1000 / frameRate));
+  }, function(err){
+    console.log(err);
+  }); 
+
+  var onFrame = function(canvas) {
+    var context = canvas.getContext("2d");
+    var canvasWidth = canvas.width;
+    var canvasHeight = canvas.height;
+
+    var imageData = context.getImageData(0, 0, canvasWidth, canvasHeight);
+
+    var color = getColorAtOffset(imageData.data, 3000);
+    colorDiv.css('background-color', 'rgb('+color.red+','+color.green+','+color.blue+')');
+    for (var y = 0; y < canvasHeight; y += 2) { // every other row because letters are not square
+      for (var x = 0; x < canvasWidth; x++) {
+        // get each pixel's brightness and output corresponding character
+        var offset = (y * canvasWidth + x) * 4;
+        var color = getColorAtOffset(imageData.data, offset);
+        
+      }
+    }
+  };
+
+  var getColorAtOffset = function(data, offset) {
+    return {
+      red: data[offset],
+      green: data[offset + 1],
+      blue: data[offset + 2],
+      alpha: data[offset + 3]
+    };
+  }
+});
